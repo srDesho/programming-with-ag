fd925dcb2af078fec17f7bd8a682be54e78fba10
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-06-03 04:33:40 -0400

    feat: inicio de la seccion 3 (Aplicacion contador - useState)

---
+++ React-y-Spring-Boot-Creando-Webapp-Full-Stack/React/3-counter-app/src/CounterApp.jsx
@@ -0,0 +1,19 @@
+// ¿Qué son los hooks?
+// Funciones especiales de React que permiten usar estado y otras características
+// sin necesidad de crear clases. Se usan directamente en componentes funcionales.
+
+// Permiten gestionar el estado (useState), efectos secundarios (useEffect),
+// y otros comportamientos reactivos de forma modular.
+
+// Cada hook se ejecuta en el mismo orden en cada renderizado,
+// y solo afecta al componente que lo usa, evitando renders innecesarios.
+
+// También facilitan la reutilización de lógica de negocio entre componentes.
+
+
+export const CounterApp = () => {
+    return 
+    <>
+    
+    </>
+}

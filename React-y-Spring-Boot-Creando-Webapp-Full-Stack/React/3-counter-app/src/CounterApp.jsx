86d9af7dea6e43f873e5bde85867f600f6b886e2
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-06-04 05:52:11 -0400

    feat: Implementando hook useState

--- React-y-Spring-Boot-Creando-Webapp-Full-Stack/React/3-counter-app/src/CounterApp.jsx
+++ React-y-Spring-Boot-Creando-Webapp-Full-Stack/React/3-counter-app/src/CounterApp.jsx
@@ -2,6 +2,8 @@
 // Funciones especiales de React que permiten usar estado y otras características
 // sin necesidad de crear clases. Se usan directamente en componentes funcionales.
 
+import { useState } from "react";
+
 // Permiten gestionar el estado (useState), efectos secundarios (useEffect),
 // y otros comportamientos reactivos de forma modular.
 
@@ -10,10 +12,23 @@
 
 // También facilitan la reutilización de lógica de negocio entre componentes.
 
-
 export const CounterApp = () => {
-    return 
-    <>
+
+    // Creamos nuestro hook counter que será llamada en el callback de onClick
+    const [counter, setCounter] = useState(0); // Definicimos un valor inicial en nuesto hook porque sino sería undefined.
     
+    const incrementCounter = () => {
+        // setCounter(counter + 1);
+        // Otra manera de hacerlo es con callback (RECOMENDADO).
+        setCounter(c => c + 1);
+    }
+    return (
+    <>
+        <h2>El valor del contador es {counter}</h2>
+        {/* <button onClick={() => incrementCounter()}>Incrementar Contador +1</button> */}
+        {/* Podemos resumir la línea con lo sgte. */}
+        <button onClick={incrementCounter}>Incrementar contador +1</button>
+        
     </>
+    );
 }
